d8ec1b5c3a40587c00ff86c2fffb5b3656d5162f
Steven Vandevelde <steven.vandevelde@hey.com>
2023-12-21 17:18:06 +0100

    feat: expose errors via entry point

--- packages/nest/src/index.ts
+++ packages/nest/src/index.ts
@@ -2,6 +2,7 @@ export { CID } from 'multiformats/cid'
 
 export * from './app-info.js'
 export * from './class.js'
+export * from './errors.js'
 export * from './events.js'
 export * from './root-tree.js'
 export * from './root-tree/basic.js'
